120ae0ad2387ea38af16c1fc0df1ce8d978308e1
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-19 15:40:05 -0300

    Defined new operation for view index and modified template index for suport.

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    receitas: dict = {
+        1: 'Lasanha',
+        2: 'Sopa de legumes',
+        3: 'Soverte',
+        4: 'Bolo de chocolate',
+    }
+    context: dict = {
+        'nome_das_receitas': receitas,
+    }
+
+    return render(request, 'index.html', context=context)
 
 
 def receita(request):
