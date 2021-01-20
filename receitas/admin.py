964cded01bd73c04606fb605f0cd83eaefb7f450
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-20 11:21:24 -0300

    Added atribute pessoa to Receita in models. Configured search and presentation of atributes in ReceitaAdmin.

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,8 +4,8 @@ from receitas.models import Receita
 
 @admin.register(Receita)
 class ReceitaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'rendimento')
+    list_display = ('id', 'nome_receita', 'pessoa', 'categoria', 'rendimento')
     list_display_links = ('id', 'nome_receita')
-    search_fields = ('nome_receita',)
+    search_fields = ('nome_receita', 'pessoa__nome',)
     list_filter = ('categoria',)
     list_per_page = 2
