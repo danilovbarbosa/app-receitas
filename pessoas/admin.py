d1eb6ae1406a10ee76f6ef685bfb507d74a977e1
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-20 10:46:44 -0300

    Created app Pessoas and defined model and adminModel Pessoa.

---
+++ pessoas/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from pessoas.models import Pessoa
+
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+    list_display_links = ('nome', 'email')
+    search_fields = ('nome',)
