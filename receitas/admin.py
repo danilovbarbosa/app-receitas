86aec41514f2962eeb55eb002f90750973f5e825
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-20 10:00:59 -0300

    Defined new configuration for ReceitaAdmin.

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,4 +4,8 @@ from receitas.models import Receita
 
 @admin.register(Receita)
 class ReceitaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'nome_receita', 'categoria', 'rendimento')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 2
