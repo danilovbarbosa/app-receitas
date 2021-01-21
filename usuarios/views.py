7dbdfd70f93afe62461b9d9bb63859ff792e182e
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-21 14:46:55 -0300

    Defined cadastro view.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,26 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if not nome.strip():
+            print('O campo não pode ficar em branco.')
+
+        if not email.strip():
+            print('O campo não pode ficar em branco.')
+
+
+
+        return redirect('login')
     context = {
 
     }
-    return render(request, 'cadastro.html', context=context)
+    return render(request, 'usuarios/cadastro.html', context=context)
 
 def login(request):
     context = {
 
     }
-    return render(request, 'login.html', context=context)
+    return render(request, 'usuarios/login.html', context=context)
 
 def dashboard(request):
     context = {
 
     }
-    return render(request, 'dashboard.html', context=context)
+    return render(request, 'usuarios/dashboard.html', context=context)
 
 def logout(request):
     context = {
 
     }
-    return render(request, 'cadastro.html', context=context)
+    return render(request, 'usuarios/cadastro.html', context=context)
 
