ee6c3e703294ec3e4ef9fa663edc809adc780355
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-21 15:28:36 -0300

    Finished the cadastro view.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+def validar_nome_email(nome, email):
+    if not nome.strip():
+        print('O campo não pode ficar em branco.')
+        return redirect('cadastro')
+
+    if not email.strip():
+        print('O campo não pode ficar em branco.')
+        return redirect('cadastro')
+
+def verificar_igualdade_da_senha(password, password2):
+    if password != password2:
+        print('As senhas não iguais.')
+        return redirect('cadastro')
+
+def verificar_se_usuario_ja_cadastrado(email):
+    if User.objects.filter(email=email).exists():
+        print('Usuário já cadastrado.')
+        return redirect('cadastro')
 
 
 def cadastro(request):
@@ -8,18 +28,15 @@ def cadastro(request):
         password = request.POST['password']
         password2 = request.POST['password2']
 
-        if not nome.strip():
-            print('O campo não pode ficar em branco.')
+        validar_nome_email(nome, email)
+        verificar_igualdade_da_senha(password, password2)
+        verificar_se_usuario_ja_cadastrado(email)
 
-        if not email.strip():
-            print('O campo não pode ficar em branco.')
-
-
+        usuario: User = User.objects.create_user(username=nome, email=email, password=password)
+        usuario.save()
 
         return redirect('login')
-    context = {
-
-    }
+    context = {}
     return render(request, 'usuarios/cadastro.html', context=context)
 
 def login(request):
