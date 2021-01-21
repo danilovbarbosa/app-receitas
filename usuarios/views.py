8594b70c601c3a40a64b68c5d9f5d0962ab9639b
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-21 11:08:59 -0300

    Renamed template files. Created views and templates for usuarios app.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cadastro(request):
+    context = {
+
+    }
+    return render(request, 'cadastro.html', context=context)
+
+def login(request):
+    context = {
+
+    }
+    return render(request, 'login.html', context=context)
+
+def dashboard(request):
+    context = {
+
+    }
+    return render(request, 'dashboard.html', context=context)
+
+def logout(request):
+    context = {
+
+    }
+    return render(request, 'cadastro.html', context=context)
+
